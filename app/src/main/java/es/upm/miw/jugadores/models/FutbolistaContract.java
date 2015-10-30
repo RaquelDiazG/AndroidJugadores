0aa182fc5df08186b3c64830edc7ff3eb4f3f9a3
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-10-30 17:12:24 +0100

    Creada clase FutbolistaContract

---
+++ app/src/main/java/es/upm/miw/jugadores/models/FutbolistaContract.java
@@ -0,0 +1,17 @@
+package es.upm.miw.jugadores.models;
+
+import android.provider.BaseColumns;
+
+public abstract class FutbolistaContract {
+
+    public static class TablaFutbolista implements BaseColumns {
+
+        public static final String TABLE_NAME="futbolistas";
+
+        public static final String COL_NAME_NOMBRE="nombre";
+        public static final String COL_NAME_DORSAL="dorsal";
+        public static final String COL_NAME_LESIONADO="lesionado";
+        public static final String COL_NAME_EQUIPO="equipo";
+        public static final String COL_NAME_URL_IMAGEN="url_imagen";
+    }
+}
